0e14c43ba85698b9139bcda597feb3c5cf20e2d4
jjunest <jjunest@gmail.com>
2021-09-04 19:24:05 +0900

    Fullvesting DB생성

--- TheaterWinBook/admin.py
+++ TheaterWinBook/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post,TheaterWinBookRecord,TheaterWinQuestion, TheaterWinQuestionInfo,TheaterWinQuestionReply, Full_Chatting_Message, TheaterWinBookRecordInfo, TheaterWinBookRecordReply
+from .models import Post,TheaterWinBookRecord,TheaterWinQuestion, TheaterWinQuestionInfo,TheaterWinQuestionReply, Full_Chatting_Message, TheaterWinBookRecordInfo, TheaterWinBookRecordReply, StockSummaryKr
 
 admin.site.register(Post)
 admin.site.register(TheaterWinBookRecord)
@@ -11,3 +11,4 @@ admin.site.register(TheaterWinQuestionReply)
 admin.site.register(Full_Chatting_Message)
 admin.site.register(TheaterWinBookRecordInfo)
 admin.site.register(TheaterWinBookRecordReply)
+admin.site.register(StockSummaryKr)
--- TheaterWinBook/models.py
+++ TheaterWinBook/models.py
@@ -5,6 +5,75 @@ from django.utils.datetime_safe import datetime
 # from tinymce import HTMLField
 
 
+class StockSummaryKr(models.Model):
+    bat_time = models.DateTimeField(default=datetime.now, blank = False)
+    info_date = models.DateField(default=datetime.now, blank=False)
+    stock_code = models.SmallIntegerField(default=1, blank=False)
+    stock_name = models.CharField(max_length=20, blank=True)
+
+    class Meta:
+        unique_together = ("info_date","stock_code")
+    # stock_country =
+    # vesting_type
+    # vesting_type_detail
+    # stock_market_sum
+    # stock_share_total_num
+    # stock_first_price
+    # stock_foreign_share_max
+    # stock_foreign_share_num
+    # stock_foreign_share_percent
+    # stock_maxprice_year
+    # stock_lowprice_year
+    # stock_per
+    # stock_eps
+    # stock_per_guess
+    # stock_eps_guess
+    # stock_pbr
+    # stock_bps
+    # stock_allocation_ratio
+    # stock_similar_per
+    # stock_now
+    # stock_close
+    # stock_open
+    # stock_high
+    # stock_low
+    # stock_volume_share
+    # stock_volume_money
+    # stock_trading_sum_foreign_
+    # stock_trading_sum_agency
+    # stock_trading_sum_ant
+    # stock_agency_buy_top1
+    # stock_agency_buy_top1_vol
+    # stock_agency_buy_top2
+    # stock_agency_buy_top2_vol
+    # stock_agency_buy_top3
+    # stock_agency_buy_top3_vol
+    # stock_agency_buy_top4
+    # stock_agency_buy_top4_vol
+    # stock_agency_buy_top5
+    # stock_agency_buy_top5_vol
+    # stock_agency_sell_top1
+    # stock_agency_sell_top1_vol
+    # stock_agency_sell_top2
+    # stock_agency_sell_top2_vol
+    # stock_agency_sell_top3
+    # stock_agency_sell_top3_vol
+    # stock_agency_sell_top4
+    # stock_agency_sell_top4_vol
+    # stock_agency_sell_top5
+    # stock_agency_sell_top5_vol
+    # etc1_string
+    # etc2_string
+    # etc3_string
+    # etc4_string
+    # etc5_string
+    # etc1_int
+    # etc2_int
+    # etc3_int
+    # etc4_int
+    # etc5_int
+
+
 class TheaterWinQuestion(models.Model):
     # 토계부 질문게시판용 필드 모델
     writing_date = models.DateField(default=datetime.now, blank=False)
