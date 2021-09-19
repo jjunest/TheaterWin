70166c9c01ca9cd42986fcf135a1023d9f8dc45d
jjunest <jjunest@gmail.com>
2021-09-19 10:17:05 +0900

    아파치 배포용 (wsgi.py 변경)

--- TheaterWin/wsgi.py
+++ TheaterWin/wsgi.py
@@ -7,10 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TheaterWin.settings')
+# 개발 용
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TheaterWin.settings')
+#
+# application = get_wsgi_application()
 
+
+# 배포용 (apache2)
+path = os.path.abspath(__file__+'/../..')
+if path not in sys.path :
+        sys.path.append(path)
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","TheaterWin.settings")
 application = get_wsgi_application()
