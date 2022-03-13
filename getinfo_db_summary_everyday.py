1aea2ee21ba478310cbe253cee1dc12dd4ee74be
jjunest <jjunest@gmail.com>
2022-03-13 15:39:44 +0900

    getSummaryEveryday into DB

--- getinfo_db_summary_everyday.py
+++ getinfo_db_summary_everyday.py
@@ -33,7 +33,7 @@ def get_page_content(url):
 
 # (코드참고) https://aidalab.tistory.com/29
 def get_stock_list_kor():
-    # print('this is get_stock_list_kor() start')
+    print('this is get_stock_list_kor() start')
     # 종목코드는 거래소 파일에서 읽어옴. 네이버주가총액은 etf까지 존재, 거래소파일은 fullvestapi 폴더와 동일위치
     # 운영서버 코드
     stock_list_kospi_csv = pd.read_csv("/home/fullvestapi/kospi_list.csv", encoding='euc-kr')
@@ -142,7 +142,7 @@ def get_stock_summary_info_kor(stock_list_kor) :
             #     break
             stock_code = stock_list_kor.loc[i,"stock_code"]
             stock_detail_url = stock_detail_url_temp % stock_code
-            # print(stock_detail_url)
+            print(stock_detail_url)
             stock_detail_soup = get_page_content(stock_detail_url)
             # 가져올 데이터 # (1-1)저장 날짜는 항상 저장하자
             # strptime 는 객체를 -> datetime 오브젝트로 변환, strftime는 string형으로 변환
